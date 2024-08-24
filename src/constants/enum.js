10d76dbe2381f57dd9f16a3193bb42e1ef68bfc1
toos <tophamln33@gmail.com>
2024-08-25 00:31:02 +0700

    First commit

---
+++ src/constants/enum.js
@@ -0,0 +1,255 @@
+export const ETransferType = Object.freeze({
+  LOW: 'Chuyển chậm',
+  NAPAS: 'Chuyển tiền nhanh Napas',
+  FAST: 'Chuyển tiền nhanh',
+  VIETCOMBANK: 'Chuyển tiền trong VietcomBank'
+});
+
+export const EOs = Object.freeze({
+  IOS: 'IOS/Iphone'
+});
+
+export const EFakeType = Object.freeze({
+  VIETCOMBANK: 'VietcomBank',
+  VIETTINBANK: 'ViettinBank',
+  VIETINBANK: 'VietinBank'
+});
+
+export const EBank = Object.freeze({
+  VCB: {
+    context: 'VietcomBank',
+    value: 'TMCP Ngoại Thương Việt Nam'
+  },
+  CTG: {
+    context: 'VietinBank',
+    value: 'TMCP Công Thương Việt Nam'
+  },
+  TCB: {
+    context: 'Techcombank',
+    value: 'TMCP Kỹ Thương Việt Nam'
+  },
+  BIDV: {
+    context: 'BIDV',
+    value: 'TMCP Đầu Tư Và Phát Triển'
+  },
+  VARB: {
+    context: 'AgriBank',
+    value: 'Nông Nghiệp Và PT Nông Thôn'
+  },
+  NVB: {
+    context: 'Navibank',
+    value: 'TMCP Quốc Dân'
+  },
+  STB: {
+    context: 'Sacombank',
+    value: 'TMCP Sài Gòn Thương Tín'
+  },
+  ACB: {
+    context: 'ACB',
+    value: 'TMCP Á Châu'
+  },
+  MB: {
+    context: 'MBBank',
+    value: 'TMCP Quân Đội'
+  },
+  TPB: {
+    context: 'TPBank',
+    value: 'TMCP Tiên Phong'
+  },
+  SVB: {
+    context: 'Shinhan Bank',
+    value: 'TNHH MTV Shinhan Việt Nam'
+  },
+  VIB: {
+    context: 'VIB Bank',
+    value: 'TMCP Quốc Tế Việt Nam'
+  },
+  VPB: {
+    context: 'VPBank',
+    value: 'TMCP Việt Nam Thịnh Vượng'
+  },
+  SHB: {
+    context: 'SHB',
+    value: 'TMCP Sài Gòn Hà Nội'
+  },
+  EIB: {
+    context: 'Eximbank',
+    value: 'TMCP Xuất Nhập Khẩu Việt Nam'
+  },
+  BVB: {
+    context: 'BaoVietBank',
+    value: 'TMCP Bảo Việt'
+  },
+  VCCB: {
+    context: 'VietcapitalBank',
+    value: 'TMCP Bản Việt'
+  },
+  SCB: {
+    context: 'SCB',
+    value: 'TMCP Sài Gòn'
+  },
+  VRB: {
+    context: 'VietNam - Russia Bank',
+    value: 'Liên Doanh Việt Nga'
+  },
+  ABB: {
+    context: 'ABBank',
+    value: 'TMCP An Bình'
+  },
+  PVCB: {
+    context: 'PVCombank',
+    value: 'TMCP Đại Chúng Việt Nam'
+  },
+  OJB: {
+    context: 'OceanBank',
+    value: 'TM TNHH MTV Đại Dương'
+  },
+  NAB: {
+    context: 'NamA bank',
+    value: 'TMCP Nam Á'
+  },
+  HDB: {
+    context: 'HDBank',
+    value: 'TMCP Phát Triển TPHCM'
+  },
+  VB: {
+    context: 'VietBank',
+    value: 'TMCP Việt Nam Thương Tín'
+  },
+  CFC: {
+    context: 'VietCredit',
+    value: 'Công ty Tài chính Cổ Phần Tín Việt'
+  },
+  PBViệt_Nam: {
+    context: 'Public bank',
+    value: 'TNHH MTV Public Việt Nam'
+  },
+  HLB: {
+    context: 'Hongleong Bank',
+    value: 'TNHH MTV Hongleong Việt Nam'
+  },
+  PGB: {
+    context: 'PG Bank',
+    value: 'TMCP Xăng Dầu Petrolimex'
+  },
+  COB: {
+    context: 'Co.op Bank',
+    value: 'Hợp Tác'
+  },
+  CIMB: {
+    context: 'CIMB',
+    value: 'TNHH MTV CIMB Việt Nam'
+  },
+  IVB: {
+    context: 'Indovina',
+    value: 'TNHH Indovina'
+  },
+  DAB: {
+    context: 'DongABank',
+    value: 'TMCP Đông Á'
+  },
+  GPB: {
+    context: 'GPBank',
+    value: 'TM TNHH MTV Dầu Khí Toàn Cầu'
+  },
+  NASB: {
+    context: 'BacABank',
+    value: 'TMCP Bắc Á'
+  },
+  VAB: {
+    context: 'VietABank',
+    value: 'TMCP Việt Á'
+  },
+  SGB: {
+    context: 'SaigonBank',
+    value: 'TMCP Sài Gòn Công Thương'
+  },
+  MSB: {
+    context: 'Maritime Bank',
+    value: 'TMCP Hàng Hải Việt Nam'
+  },
+  LPB: {
+    context: 'LienVietPostBank',
+    value: 'TMCP Bưu Điện Liên Việt'
+  },
+  KLB: {
+    context: 'KienLongBank',
+    value: 'TMCP Kiên Long'
+  },
+  IBKHN: {
+    context: 'IBK - Ha Noi',
+    value: 'Công Nghiệp Hàn Quốc - Hà Nội'
+  },
+  WOO: {
+    context: 'Woori bank',
+    value: 'Wooribank'
+  },
+  SEAB: {
+    context: 'SeABank',
+    value: 'TMCP Đông Nam Á'
+  },
+  UOB: {
+    context: 'UOB',
+    value: 'TNHH MTV United Overseas Bank'
+  },
+  OCB: {
+    context: 'OCB',
+    value: 'TMCP Phương Đông'
+  },
+  MAFC: {
+    context: 'Mirae Asset',
+    value: 'TNHH MTV Mirae Asset (Viet Nam)'
+  },
+  KEBHANAHCM: {
+    context: 'Keb Hana - Ho Chi Minh',
+    value: 'Keb Hana - TP HCM'
+  },
+  KEBHANAHN: {
+    context: 'Keb Hana - Ha Noi',
+    value: 'Keb Hana - Hà Nội'
+  },
+  STANDARD: {
+    context: 'Standard Chartered',
+    value: 'Standard Chartered'
+  },
+  CAKE: {
+    context: 'CAKE',
+    value: 'So CAKE by VPBank'
+  },
+  Ubank: {
+    context: 'Ubank',
+    value: 'So Ubank by VPBank'
+  },
+  NonghyupBankHN: {
+    context: 'Nonghyup Bank - HN',
+    value: 'Nonghyup - Hà Nội'
+  },
+  KBHN: {
+    context: 'Kookmin - HN',
+    value: 'Kookmin - Hà Nội'
+  },
+  KBHCM: {
+    context: 'Kookmin - HCM',
+    value: 'Kookmin - TP. HCM'
+  },
+  DBSHCM: {
+    context: 'DBS - HCM',
+    value: 'DBS - TP. HCM'
+  },
+  CBBank: {
+    context: 'CBBank',
+    value: 'TM TNHH MTV Xây Dựng'
+  },
+  KBankHCM: {
+    context: 'KBank - HCM',
+    value: 'Đại Chúng TNHH Kasikornbank'
+  },
+  HSBC: {
+    context: 'HSBC',
+    value: 'TNHH MTV HSBC Việt Nam'
+  },
+  Timo: {
+    context: 'Timo',
+    value: 'So Timo'
+  }
+});
